26c0b7ed8dd49b363c294dfc2c55f8b75ff8aa56
plg <patrykdlugosz99@gmail.com>
2020-12-19 00:16:05 +0100

    Basic checking for updates

---
+++ BP_BenchProject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chmod +x ./BP_* #making all BP_ project files as executeable
+
+./BP_CheckCurrentVersion.sh	#check current system version
+
+cat ./BPD_SystemVersionData.txt | grep "NAME="
+cat ./BPD_SystemVersionData.txt | grep "VERSION="
+
+
+#./BP_CheckForSoftwares.sh #look for specific installed softwares
+
---
+++ BP_CheckCurrentVersion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for FILE in /etc/*release; do
+  cat "$FILE" | grep "NAME=" > ./BPD_SystemVersionData.txt
+  cat "$FILE" | grep "VERSION=" >> ./BPD_SystemVersionData.txt	#check current system version
+done
