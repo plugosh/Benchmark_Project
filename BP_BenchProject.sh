fec359f8daed2807bd226e3a0492ffca76f5d31f
plg <patrykdlugosz99@gmail.com>
2020-12-19 15:06:35 +0100

    Updates to version checking

--- BP_BenchProject.sh
+++ BP_BenchProject.sh
@@ -6,6 +6,6 @@ chmod +x ./BP_* #making all BP_ project files as executeable
 cat ./BPD_SystemVersionData.txt | grep "NAME="
 cat ./BPD_SystemVersionData.txt | grep "VERSION="
 
-
 #./BP_CheckForSoftwares.sh #look for specific installed softwares
 
+./BP_CheckForSoftwares.sh
--- BP_CheckForSoftwares.sh
+++ BP_CheckForSoftwares.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+apt-cache policy wireshark > BPD_SoftwaresData.txt
+echo "#####################################" >> BPD_SoftwaresData.txt
