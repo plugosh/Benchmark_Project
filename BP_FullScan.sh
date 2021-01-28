b6bfd364b6d3c8e248cc9415dad77f0437877ab8
plg <patrykdlugosz99@gmail.com>
2021-01-28 15:04:15 +0100

    checking how many programs can u install - all

--- BP_FullScan.sh
+++ BP_FullScan.sh
@@ -9,8 +9,27 @@ dpkg -l | cut -d ' ' -f3-3 >> ./BPD_FullList.txt
 
 Linecounter=`wc -l < ./BPD_FullList.txt`
 
+possible=0
+impossible=0
+
 for (( i = 7; i <$Linecounter; i++ )); do
 
-	sudo apt-get install `sed -n ${i}p ./BPD_FullList.txt`
+	#`sed -n ${i}p ./BPD_FullList.txt`
+
+	COutput=$(apt-cache policy `sed -n ${i}p ./BPD_FullList.txt`)
+	#echo "$COutput"
+#num=$(($num1 + $num2))
+	if [[ -z "$COutput" ]]; then
+		impossible=$(($impossible+1))
+	else
+		
+		possible=$(($possible+1))
+	fi
+	#sudo apt-get install `sed -n ${i}p ./BPD_FullList.txt`
+
+	#echo "$i"
 
 done
+
+echo "$possible"
+echo "$impossible"
